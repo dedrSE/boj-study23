579a7e601dd77f820b7ed86c60ac200f377c62b5
dedrSE <dedr_s2@naver.com>
2025-11-10 14:39:23 +0900

    feat(#1): BOJ-2745 진법 변환 최종 제출-정답

--- baekjoon/2745/solution.cpp
+++ baekjoon/2745/solution.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <math.h>
 
-using namesapce std;
+using namespace std;
 
 int main() {
+    string input_num;
+    int base_num;
+    int input_size;
+    int result = 0;
 
+    cin >> input_num >> base_num;
+
+    input_size = input_num.size();
+    
+    for(int i = input_size-1 ; i >=0 ; i--){
+        char c = input_num.at(i);
+        int tmp;
+        if(isalpha(c)){
+            tmp = (tolower(c) - 'a' + 10);
+        } else {
+            tmp = c - '0';
+        }
+        // cout << tmp << "*" << pow(base_num, (input_size-i-1)) << endl;
+        tmp = (tmp * pow(base_num, (input_size-i-1)));
+        // cout << tmp << endl;
+        result += tmp;
+    }
+
+    cout << result;
 }
